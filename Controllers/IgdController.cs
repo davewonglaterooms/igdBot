5e61610c5b3555865418537ded4272253c4f3b41
Ian Cotterill <iancotterill@gmail.com>
2014-11-12 15:11:50 +0000

    Store name and store current level of chips

--- Controllers/IgdController.cs
+++ Controllers/IgdController.cs
@@ -12,6 +12,8 @@ namespace igdBot.Controllers
         private string BlindStorePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BlindStore.txt");
         private string CardStorePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CardStore.txt");
         private string OppoMovePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "OppoMoveStore.txt");
+        private string OppoNamePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "OppoNameStore.txt");
+        private string ChipsLeftPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ChipsLeftStore.txt");
 
         [HttpGet]
         public string Move()
@@ -22,47 +24,47 @@ namespace igdBot.Controllers
 
             var blind = GetBlindValue();
 
-            if (oppBet > 30)
-                move = "FOLD";
-            else
+            move = oppBet > 30 ? "FOLD" : MoveBasedOnCard(blind);
+
+            logger.Info(string.Format("Move Played: {0}", move));
+            return move;
+        }
+
+        private string MoveBasedOnCard(bool blind)
+        {
+            var move = "BET";
+            if (System.IO.File.Exists(CardStorePath))
             {
-                if (System.IO.File.Exists(CardStorePath))
+                using (var sr = new StreamReader(System.IO.File.Open(CardStorePath,
+                                                                     FileMode.Open)))
                 {
-                    using (var sr = new StreamReader(System.IO.File.Open(CardStorePath,
-                                                                         FileMode.Open)))
+                    var card = sr.ReadLine();
+
+                    if (card == null)
+                        return move;
+
+                    if (card.Contains("A"))
+                        move = "BET:30";
+
+                    if (card.Contains("Q") || card.Contains("K"))
+                        move = "BET:5";
+
+                    int cardNum;
+                    if (int.TryParse(card, out cardNum))
                     {
-                        var card = "";
-                        card = sr.ReadLine();
-
-                        if (card == null)
-                            return move;
-
-                        if (card.Contains("A"))
-                            move = "BET:30";
-
-                        if (card.Contains("Q") || card.Contains("K"))
-                            move = "BET:5";
-
-                        int cardNum;
-                        if (int.TryParse(card, out cardNum))
+                        if (blind)
                         {
-                            if (blind)
-                            {
-                                if (cardNum < 5)
-                                    move = "FOLD";
-                            }
-                            else
-                            {
-                                if (cardNum < 9)
-                                    move = "FOLD";
-                            }
-
+                            if (cardNum < 5)
+                                move = "FOLD";
+                        }
+                        else
+                        {
+                            if (cardNum < 9)
+                                move = "FOLD";
                         }
                     }
                 }
             }
-
-            logger.Info(string.Format("Move Played: {0}", move));
             return move;
         }
 
@@ -116,6 +118,17 @@ namespace igdBot.Controllers
         [HttpPost]
         public string Start(string OPPONENT_NAME, int STARTING_CHIP_COUNT, int HAND_LIMIT)
         {
+            InitialiseFiles();
+            StoreOppoName(OPPONENT_NAME);
+            StoreChipsLeft(STARTING_CHIP_COUNT.ToString());
+
+            string msg = string.Format("Opponent_Name:{0}, Starting_Chip_Count:{1}, Hand_Limit:{2}", OPPONENT_NAME, STARTING_CHIP_COUNT, HAND_LIMIT);
+            Log("start", msg);
+            return "starttest";
+        }
+
+        private void InitialiseFiles()
+        {
             using (var sr = new StreamWriter(System.IO.File.Open(CardStorePath, FileMode.Create)))
             {
                 sr.Write("");
@@ -131,9 +144,15 @@ namespace igdBot.Controllers
                 sr.Write("");
             }
 
-            string msg = string.Format("Opponent_Name:{0}, Starting_Chip_Count:{1}, Hand_Limit:{2}", OPPONENT_NAME, STARTING_CHIP_COUNT, HAND_LIMIT);
-            Log("start", msg);
-            return "starttest";
+            using (var sr = new StreamWriter(System.IO.File.Open(OppoNamePath, FileMode.Create)))
+            {
+                sr.Write("");
+            }
+
+            using (var sr = new StreamWriter(System.IO.File.Open(ChipsLeftPath, FileMode.Create)))
+            {
+                sr.Write("");
+            }
         }
 
         private void Log(string action, string command)
@@ -159,6 +178,7 @@ namespace igdBot.Controllers
                     StoreOppoMove(DATA);
                     break;
                 case "RECEIVE_CHIPS":
+                    StoreChipsLeft(DATA);
                     break;
                 case "OPPONENT_CARD":
                     break;
@@ -167,6 +187,47 @@ namespace igdBot.Controllers
             Log("Update", string.Format("Command: {0}, Data: {1}", COMMAND ?? "NoCommand", DATA ?? "NoData"));
 
             return "done";
+        }
+
+        private void StoreChipsLeft(string chips)
+        {
+            var existing = 0;
+            using (var sr = new StreamReader(System.IO.File.Open(ChipsLeftPath, FileMode.Open)))
+            {
+                var existingChips = sr.ReadLine();
+
+                if (existingChips != null)
+                {
+                    
+                    if (!int.TryParse(existingChips, out existing))
+                    {
+                        existing = 0;
+                    }
+                }
+            }
+
+            int received;
+            if (!int.TryParse(chips, out received))
+            {
+                received = 0;
+            }
+
+            var currentChips = existing + received;
+
+            Log("Chips", string.Format("Current Chips: {0}", currentChips));
+
+            using (var sr = new StreamWriter(System.IO.File.Open(ChipsLeftPath, FileMode.Create)))
+            {
+                sr.WriteLine(currentChips);
+            }
+        }
+
+        private void StoreOppoName(string name)
+        {
+            using (var sr = new StreamWriter(System.IO.File.Open(OppoNamePath, FileMode.Create)))
+            {
+                sr.WriteLine(name);
+            }
         }
 
         private void StoreOppoMove(string data)
@@ -191,22 +252,6 @@ namespace igdBot.Controllers
             {
                 sr.WriteLine(data);
             }
-        }
-
-        [HttpGet]
-        public JsonResult GetLogs()
-        {
-            var logContent = "";
-            using (var sr = new StreamReader(System.IO.File.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogCommands.txt"), FileMode.Open)))
-            {
-                logContent = sr.ReadToEnd();
-            }
-            
-            return new JsonResult
-            {
-                Data = logContent,
-                JsonRequestBehavior = JsonRequestBehavior.AllowGet
-            };
         }
     }
 }
