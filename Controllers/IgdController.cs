433b027e3543bcd2ad93cff47c18c3402eb5200e
Ian Cotterill <iancotterill@gmail.com>
2014-11-12 12:30:35 +0000

    log4net

---
+++ Controllers/IgdController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using log4net;
+using log4net.Core;
+
+namespace igdBot.Controllers
+{
+    public class IgdController : Controller
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(IgdController));
+
+        [HttpGet]
+        public string Move()
+        {
+            return "BET";
+        }
+
+        [HttpPost]
+        public string Start(string OPPONENT_NAME, int STARTING_CHIP_COUNT, int HAND_LIMIT)
+        {
+            string msg = string.Format("Opponent_Name:{0}, Starting_Chip_Count:{1}, Hand_Limit:{2}", OPPONENT_NAME, STARTING_CHIP_COUNT, HAND_LIMIT);
+            Log("start", msg);
+            return "starttest";
+        }
+
+        private void Log(string action, string command)
+        {
+            logger.Info(command);
+
+            //using (var sr = new StreamWriter(System.IO.File.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogCommands.txt"), FileMode.Append)))
+            //{
+            //    sr.WriteLine(command);
+            //}
+        }
+
+        [HttpPost]
+        public string Update(string COMMAND, string DATA)
+        {
+            Log("Update", string.Format("Command: {0}, Data: {1}", COMMAND ?? "NoCommand", DATA ?? "NoData"));
+
+            return "done";
+        }
+
+        [HttpGet]
+        
+        public JsonResult GetLogs()
+        {
+            return new JsonResult()
+            {
+                //Data = JsonConvert.SerializeObject(_log),
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
---
+++ Tests/AllOfTheTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using igdBot.Controllers;
+
+namespace igdBot.Tests
+{
+    [TestFixture]
+    public class AllOfTheTests
+    {
+        [Test, Ignore]
+        public void If_card_dealt_is_10_then_next_get_should_be_Bet_10()
+        {
+            var controller = new IgdController();
+
+            controller.Update("CARD", "10");
+
+            var response = controller.Move();
+
+            Assert.That(response, Is.EqualTo("BET:10"));
+        }
+
+        [Test, Ignore]
+        public void If_card_dealt_is_J_then_next_get_should_be_Bet_10()
+        {
+            var controller = new IgdController();
+
+            controller.Update("CARD", "J");
+
+            var response = controller.Move();
+
+            Assert.That(response, Is.EqualTo("BET:10"));
+        }
+
+        [Test, Ignore]
+        public void If_card_dealt_is_Q_then_next_get_should_be_Bet_10()
+        {
+            var controller = new IgdController();
+
+            controller.Update("CARD", "Q");
+
+            var response = controller.Move();
+
+            Assert.That(response, Is.EqualTo("BET:10"));
+        }
+
+        [Test, Ignore]
+        public void If_card_dealt_is_K_then_next_get_should_be_Bet_10()
+        {
+            var controller = new IgdController();
+
+            controller.Update("CARD", "K");
+
+            var response = controller.Move();
+
+            Assert.That(response, Is.EqualTo("BET:10"));
+        }
+    }
+}
