845f9d90a0a8d5f14e94bc825044f55d2d96b61a
Dave Wong <wongcheming@bitbucket.org>
2014-11-12 10:26:35 +0000

    Added logging for the start

--- Controllers/Default1Controller.cs
+++ Controllers/Default1Controller.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
 
 namespace igdBot.Controllers
 {
@@ -13,9 +15,19 @@ namespace igdBot.Controllers
         }
 
         [HttpPost]
-        public string Start()
+        public string Start(string OPPONENT_NAME, int STARTING_CHIP_COUNT, int HAND_LIMIT)
         {
+            Log(string.Format("Game Started with player {0} with tokens {1} and limit {2}", OPPONENT_NAME, STARTING_CHIP_COUNT, HAND_LIMIT));
+            
             return "start";
+        }
+
+        private void Log(string command)
+        {
+            using (var sr = new StreamWriter(System.IO.File.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogCommands.txt"), FileMode.Append)))
+            {
+                sr.WriteLine(command);
+            }
         }
 
         [HttpPost]
