293d32107cc567a9a11811327e564ebd358c4d2e
Dave Wong <wongcheming@bitbucket.org>
2014-11-12 09:45:57 +0000

    First Commit

---
+++ Controllers/Default1Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace igdBot.Controllers
+{
+    public class IgdController : Controller
+    {
+        //
+        // 
+        [HttpGet]
+        public string Move()
+        {
+            return "Get";
+        }
+    }
+}
